2b79fc661dc852adbf2b20bf0be7d2da415506c2
Everest Luong <101264127+hikalg@users.noreply.github.com>
2025-08-31 08:28:01 +1000

    refactored code

--- skillaroo/team.py
+++ skillaroo/team.py
@@ -6,31 +6,33 @@ import player as p
 from colorama import Fore
 
 class Team:
-    team_name = ""
-    players = []
-    avg_rating = 0
-    team_rating = 0
-    wins = 0
-    matches = 0
+    team_name: str = ""
+    players: list[p.Player] = []
+    avg_rating: int = 0
+    team_ratin: int = 0
+    wins: int = 0
+    matches: int = 0
     
-    def __init__(self, team_name: str, *players: p.Player) -> None:
-        total_rating = 0
-        players_queue = []
-        avg_rating = 0
+    def __init__(self, *, team_name: str = "noname", players: list[p.Player] = [], min_team_size: int = 1, max_team_size: int = 5) -> None:
+        total_rating: int = 0
+        players_queue: list[p.Player] = []
+        avg_rating: int = 0
         
-        # Checks for team size, else compares to default values
-        if (len(players) < int(os.getenv("MIN_TEAM_SIZE") or 1)):
-            print (f"{Fore.RED}Team not created: Team size lower than minimum")
-            pass
+        # if (not team_name)
         
-        # Checks for team size, else compares to default values
-        if (len(players) > int(os.getenv("MAX_TEAM_SIZE") or 5)):
-            print (f"{Fore.RED}Team not created: Team size larger than maximum")
-            pass
+        # # Checks for team size, else compares to default values
+        # if (len(players) < int(os.getenv("MIN_TEAM_SIZE") or 1)):
+        #     print (f"{Fore.RED}Team not created: Team size lower than minimum")
+        #     pass
+        
+        # # Checks for team size, else compares to default values
+        # if (len(players) > int(os.getenv("MAX_TEAM_SIZE") or 5)):
+        #     print (f"{Fore.RED}Team not created: Team size larger than maximum")
+        #     pass
         
         # Checks for length
         
-        
+
         for player in players:
             players_queue.append(player)
             
@@ -38,6 +40,7 @@ class Team:
             total_rating = total_rating + player.rating
             
         avg_rating = total_rating / len(players_queue)
+        
         self.avg_rating = avg_rating
         self.team_name = team_name
         
